3116224551900f50eb25b17d6c6c44abf9402480
Avishagp <44115709+Avishagp@users.noreply.github.com>
2020-03-02 18:04:40 +0200

    Add weather and bus widgets

--- Wade.java
+++ Wade.java
@@ -1,5 +1,12 @@
 public class Wade {
     public static void main(String[] args) {
-        System.out.println("hi!");
+        WeatherWidget weatherWidget = new WeatherWidget();
+        BusWidget busWidget = new BusWidget();
+        String urlBus = "https://bus.gov.il/WebApi/api/passengerinfo/GetRealtimeBusLineListByBustop/26290/he/false";
+        String urlWeather = "http://api.openweathermap.org/data/2.5/weather?q="
+                + "Ramat Gan" +
+                "&appid=ba602af69e087755dc712a9ec9f29e71&units=metric";
+        weatherWidget.showWeather(urlWeather);
+        busWidget.showBusData(urlBus);
     }
 }
